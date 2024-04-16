4b730326d6bc6cdb8921228ae1fb456b0844a217
Masato Yamamoto <masato.yamamoto99@gmail.com>
2024-04-16 23:34:59 +0900

    fix:isascii-add-header

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: masayama <masayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 21:59:50 by masayama          #+#    #+#             */
-/*   Updated: 2024/04/16 22:05:16 by masayama         ###   ########.fr       */
+/*   Updated: 2024/04/16 23:34:29 by masayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
+int	ft_isascii(int c);
 
 #endif

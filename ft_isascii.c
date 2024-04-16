74da4715d4874f3d1fe36bb8f3850051f7188c4e
Masato Yamamoto <masato.yamamoto99@gmail.com>
2024-04-16 23:32:23 +0900

    featcreate-isascii

---
+++ ft_isascii.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: masayama <masayama@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 22:35:24 by masayama          #+#    #+#             */
+/*   Updated: 2024/04/16 23:31:01 by masayama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isascii(int c)
+{
+	return (0 <= c && c <= 127);
+}
+
+// #include <stdio.h>
+// #include <ctype.h>
+
+// int main(void)
+// {
+// 	printf("-----isascii-----\n");
+// 	printf("-1  = %d\n", isascii(-1));
+// 	printf("0   = %d\n", isascii(0));
+// 	printf("1   = %d\n", isascii(1));
+// 	printf("127 = %d\n", isascii(127));
+// 	printf("128 = %d\n", isascii(128));
+// 	printf("-----ft_isascii-----\n");
+// 	printf("-1  = %d\n", ft_isascii(-1));
+// 	printf("0   = %d\n", ft_isascii(0));
+// 	printf("1   = %d\n", ft_isascii(1));
+// 	printf("127 = %d\n", ft_isascii(127));
+// 	printf("128 = %d\n", ft_isascii(128));
+// }

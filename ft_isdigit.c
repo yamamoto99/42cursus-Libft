ca756b6b8a9c607d21a6bdbffe50c8f9e6348e95
Masato Yamamoto <masato.yamamoto99@gmail.com>
2024-04-16 20:56:12 +0900

    fix:main=commentout

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: masayama <masayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 20:44:37 by masayama          #+#    #+#             */
-/*   Updated: 2024/04/16 20:48:29 by masayama         ###   ########.fr       */
+/*   Updated: 2024/04/16 20:55:43 by masayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ int	ft_isdight(char c)
 	return (0);
 }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int	main(void)
-{
-	printf("0 = %d\n", ft_isdight('1'));
-	printf("9 = %d\n", ft_isdight('1'));
-	printf("a = %d\n", ft_isdight('a'));
-	printf("z = %d\n", ft_isdight('z'));
-	printf("A = %d\n", ft_isdight('A'));
-	printf("Z = %d\n", ft_isdight('Z'));
-	printf("* = %d\n", ft_isdight('*'));
-}
+// int	main(void)
+// {
+// 	printf("0 = %d\n", ft_isdight('1'));
+// 	printf("9 = %d\n", ft_isdight('1'));
+// 	printf("a = %d\n", ft_isdight('a'));
+// 	printf("z = %d\n", ft_isdight('z'));
+// 	printf("A = %d\n", ft_isdight('A'));
+// 	printf("Z = %d\n", ft_isdight('Z'));
+// 	printf("* = %d\n", ft_isdight('*'));
+// }

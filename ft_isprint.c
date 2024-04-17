888244e3ab95f5927a00fb4892f3f65018b22f29
Masato Yamamoto <masato.yamamoto99@gmail.com>
2024-04-17 11:38:54 +0900

    feat:create-isprint

---
+++ ft_isprint.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: masayama <masayama@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 11:20:21 by masayama          #+#    #+#             */
+/*   Updated: 2024/04/17 11:38:01 by masayama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isprint(int c)
+{
+	return (32 <= c && c <= 126);
+}
+
+// #include <stdio.h>
+// #include <ctype.h>
+
+// int	main(void)
+// {
+// 	printf("-----isprint-----\n");
+// 	printf("9   = %d\n", isprint(9));
+// 	printf("31  = %d\n", isprint(31));
+// 	printf("32  = %d\n", isprint(32));
+// 	printf("126 = %d\n", isprint(126));
+// 	printf("127 = %d\n", isprint(127));
+// 	printf("-----ft_isprint-----\n");
+// 	printf("9   = %d\n", ft_isprint(9));
+// 	printf("31  = %d\n", ft_isprint(31));
+// 	printf("32  = %d\n", ft_isprint(32));
+// 	printf("126 = %d\n", ft_isprint(126));
+// 	printf("127 = %d\n", ft_isprint(127));
+// }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: masayama <masayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 21:59:50 by masayama          #+#    #+#             */
-/*   Updated: 2024/04/16 23:34:29 by masayama         ###   ########.fr       */
+/*   Updated: 2024/04/17 11:38:28 by masayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
+int	ft_isprint(int c);
 
 #endif

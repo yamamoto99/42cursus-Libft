d2348e039f512c7671450905135c100ff47f4254
Masato Yamamoto <masato.yamamoto99@gmail.com>
2024-04-18 23:12:10 +0900

    fix:norm

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: masayama <masayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 22:31:03 by masayama          #+#    #+#             */
-/*   Updated: 2024/04/18 22:59:45 by masayama         ###   ########.fr       */
+/*   Updated: 2024/04/18 23:11:11 by masayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,16 +36,20 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 // 	printf("-----strlcpy-----\n");
 // 	char dst[10] = "000000000";
 // 	char src[10] = "123456";
-// 	printf("src size = %d\nreturn value = %lu\ncopy src to dst = %s\n", 7, strlcpy(dst, src, 0), dst);
-// 	printf("src size = %d\nreturn value = %lu\ncopy src to dst = %s\n", 7, strlcpy(dst, src, 5), dst);
+// 	printf("src size = %d\nreturn value = %lu\ncopy src to dst = %s\n", 7,
+// 		strlcpy(dst, src, 0), dst);
+// 	printf("src size = %d\nreturn value = %lu\ncopy src to dst = %s\n", 7,
+// 		strlcpy(dst, src, 5), dst);
 // 	strlcpy(NULL, "DW", 0);
 // 	// strlcpy("DW", NULL, 0);
 // 	// strlcpy(NULL, NULL, 0);
 // 	printf("-----ft_strlcpy-----\n");
 // 	char ft_dst[10] = "000000000";
 // 	char ft_src[10] = "123456";
-// 	printf("src size = %d\nreturn value = %lu\ncopy src to dst = %s\n", 7, ft_strlcpy(ft_dst, ft_src, 0), ft_dst);
-// 	printf("src size = %d\nreturn value = %lu\ncopy src to dst = %s\n", 7, ft_strlcpy(ft_dst, ft_src, 5), ft_dst);
+// 	printf("src size = %d\nreturn value = %lu\ncopy src to dst = %s\n", 7,
+// 		ft_strlcpy(ft_dst, ft_src, 0), ft_dst);
+// 	printf("src size = %d\nreturn value = %lu\ncopy src to dst = %s\n", 7,
+// 		ft_strlcpy(ft_dst, ft_src, 5), ft_dst);
 // 	ft_strlcpy(NULL, "DW", 0);
 // 	// ft_strlcpy("DW", NULL, 0);
 // 	// ft_strlcpy(NULL, NULL, 0);
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: masayama <masayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 21:59:50 by masayama          #+#    #+#             */
-/*   Updated: 2024/04/18 22:33:44 by masayama         ###   ########.fr       */
+/*   Updated: 2024/04/18 23:11:36 by masayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
-size_t	ft_strlcat(char * dst, const char * src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 #endif

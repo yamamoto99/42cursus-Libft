359515ec97735f548ca65419c10a96a09dc2571a
Masato Yamamoto <masato.yamamoto99@gmail.com>
2024-04-21 19:32:22 +0900

    feat:create-lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: masayama <masayama@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 18:46:33 by masayama          #+#    #+#             */
-/*   Updated: 2024/04/21 19:14:32 by masayama         ###   ########.fr       */
+/*   Updated: 2024/04/21 19:31:06 by masayama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (lst && del)
+	{
 		del(lst->content);
+		free(lst);
+	}
 }
-
